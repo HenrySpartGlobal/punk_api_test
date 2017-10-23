dc5acffffaf2040a36eb6bccbc34ea59434773d5
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-23 17:07:14 +0100

    started adding dymanic beers options

--- punk_beers/beers_helper.rb
+++ punk_beers/beers_helper.rb
@@ -1,40 +1,59 @@
-require 'httparty'
-require 'json'
+  require 'httparty'
+  require 'json'
 
-class PunkBeerService
+  class PunkBeerService
 
-  # attr_accessor :abv_gt, :abv_lt
-  #
-  # def initialize(abv_gt, abv_lt)
-  #   @abv_gt = abv_gt
-  #   @abv_lt = abv_lt
-  # end
 
-  include HTTParty
+    include HTTParty
 
-  base_uri 'https://api.punkapi.com/v2/' #adds this to the front of the URL
 
-  def single_beer_call(id)
-    single_beer_call = JSON.parse(self.class.get("/beers/#{id}").body) #need to pares so ruby and handle JS, and we will get a Hash back. This part appends to line 7.
-    single_beer_call[0]
+    def initialize
+        @uri = ""
+
+    end
+
+    base_uri 'https://api.punkapi.com/v2/' #adds this to the front of the URL
+
+    def single_beer_call(id)
+      single_beer_call = JSON.parse(self.class.get("/beers/#{id}").body) #need to pares so ruby and handle JS, and we will get a Hash back. This part appends to line 7.
+      single_beer_call[0]
+    end
+
+    def random_beers_call
+      random_beers_call = JSON.parse(self.class.get("/beers/random").body)
+      random_beers_call[0]
+    end
+
+    def all_beers (opts = {})
+      if opts.empty? == false
+        @uri << "?" #adds ? straight away at the end of the link (count), (need one less and than the amount of values added)
+        opts.each do |k,v|
+        @uri << "#{k}=#{v}"
+
+        query_response = JSON.parse(self.class.get("/beers#{@uri}").body)
+        query_response[0]
+        end
+      else
+        reponse = JSON.parse(self.class.get("/beers").body)
+        response[0]
+    end
   end
 
-  def random_beers_call
-    random_beers_call = JSON.parse(self.class.get("/beers/random").body)
-    random_beers_call[0]
-  end
-  def all_beers
-    all_beers = JSON.parse(self.class.get("/beers").body)
-    all_beers[0]
-  end
 end
+# chooseBeer = PunkBeerService.new
+# puts chooseBeer.select_beer.class
 
 
-beer = PunkBeerService.new
-puts beer.single_beer_call('1').class
+
+# https://api.punkapi.com/v2/beers?beer_name=american&abv_gt=6
+
+# beer = PunkBeerService.new
+# puts beer.single_beer_call('1').class
 #
 # beers = PunkBeerService.new
 # puts beers.random_beers_call
 #
 # beers = PunkBeerService.new
 # puts beers.all_beers
+ kc = PunkBeerService.new
+ kc.all_beers({"yeast" => "american", "abv_gt" => 4})
--- punk_beers/spec/single_beer_spec.rb
+++ punk_beers/spec/single_beer_spec.rb
@@ -4,7 +4,7 @@ describe 'test beers' do
 
 before(:each) do
   service = PunkBeerService.new
-  @beers_tests = service.single_beer_call('beers')
+  @beers_tests = service.all_beers('beers')
 end
 
   it 'all key quality values should be between 1-8' do
