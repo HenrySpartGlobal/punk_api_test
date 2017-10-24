054a3dcb0dfc3b0547eea05494f8803a89c43a36
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-24 11:59:33 +0100

    created 2 services, and have working tests

--- punk_beers/beers_helper.rb
+++ punk_beers/beers_helper.rb
@@ -2,47 +2,45 @@ require 'httparty'
 require 'json'
 
 class PunkBeerService
-
   include HTTParty
   attr_accessor :uri
 
-  def initialize
-      @uri = ""
+def initialize
+    @uri = ""
+end
+
+base_uri 'https://api.punkapi.com/v2/' #adds this to the front of the URL
+
+def single_beer_call(id)
+  single_beer_call = JSON.parse(self.class.get("/beers/#{id}").body) #need to pares so ruby and handle JS, and we will get a Hash back. This part appends to line 7.
+  single_beer_call[0]
+end
+
+def random_beers_call
+  random_beers_call = JSON.parse(self.class.get("/beers/random").body)
+  random_beers_call[0]
+end
+
+def all_beers_service
+    all = JSON.parse(self.class.get("/beers").body)
+end
+
+def select_beers (opts = {})
+  if opts.empty? == false
+    num_keys = 0
+    @uri << "?"
+    opts.each do |k,v|
+      @uri << "#{k}=#{v}"
+    while num_keys < opts.keys.length - 1
+      @uri << '&'
+      num_keys += 1
   end
-
-  base_uri 'https://api.punkapi.com/v2/' #adds this to the front of the URL
-
-  def single_beer_call(id)
-    single_beer_call = JSON.parse(self.class.get("/beers/#{id}").body) #need to pares so ruby and handle JS, and we will get a Hash back. This part appends to line 7.
-    single_beer_call[0]
-  end
-
-  def random_beers_call
-    random_beers_call = JSON.parse(self.class.get("/beers/random").body)
-    random_beers_call[0]
-  end
-
-  def all_beers_service
-      all = JSON.parse(self.class.get("/beers").body)
-
-  end
-  def select_beers (opts = {})
-    if opts.empty? == false
-      num_keys = 0
-      @uri << "?"
-      opts.each do |k,v|
-        @uri << "#{k}=#{v}"
-        while num_keys < opts.keys.length - 1
-          @uri << '&'
-          num_keys += 1
-      end
-
       query_response = JSON.parse(self.class.get("/beers#{@uri}").body)
       query_response[0]
-      end
-      else
-        response = JSON.parse(self.class.get("/beers").body)
-        response[0]
+    end
+    else
+    response = JSON.parse(self.class.get("/beers").body)
+    response[0]
     end
   end
 end
--- punk_beers/spec/single_beer_spec.rb
+++ punk_beers/spec/single_beer_spec.rb
@@ -2,6 +2,8 @@
 
   describe 'test beers' do
 
+  context 'all beers' do
+
   before(:all) do
     service = PunkBeerService.new
     @beers_tests = service.all_beers_service
@@ -12,24 +14,43 @@
       expect(i["name"]).to be_a_kind_of(String)
     end
   end
-    it 'all key values of id should be of data type Integer' do
+
+    it 'all key values of "id" should be of data type Integer' do
       @beers_tests.each do |i|
       expect(i["id"]).to be_a_kind_of(Integer)
     end
   end
-    it 'The abv value of all beers should be between 1 and 55' do
+
+    it 'The "Alcohol by volume" value of all beers should be between 1 and 55' do
       @beers_tests.each do |i|
       expect(i["abv"]).to be_between(1,55)
     end
   end
-    it 'The description should be a string data type' do
+
+    it 'The "description" should be a string data type' do
       @beers_tests.each do |i|
       expect(i["description"]).to be_a_kind_of(String)
     end
   end
-    it 'The description value of the boil volume should be an integer' do
+
+    it 'The description value of the "boil volume" should be an integer' do
       @beers_tests.each do |i|
       expect(i["boil_volume"]["value"]).to be_a_kind_of(Integer)
     end
   end
+end
+
+  context 'single beer' do
+      before(:all) do
+      service = PunkBeerService.new
+      @single_beer_test = service.single_beer_call("1")
+      end
+
+      it 'all key values of name should be of data type string' do
+        expect(@single_beer_test["name"]).to be_a_kind_of(String)
+      end
+      it 'all PH values should be between 3.2 and 5.6' do
+          expect(@single_beer_test["ph"]).to be_between(3.2,5.9)
+      end
+    end
 end
