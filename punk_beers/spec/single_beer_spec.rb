d10ddfa256bfd6a7a2c6fc5245e30db5dc0b95c9
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-24 10:49:29 +0100

    can now begin testing

--- punk_beers/spec/single_beer_spec.rb
+++ punk_beers/spec/single_beer_spec.rb
@@ -1,20 +1,35 @@
-require 'spec_helper'
+  require 'spec_helper'
 
-describe 'test beers' do
+  describe 'test beers' do
 
-before(:all) do
-  service = PunkBeerService.new
-  @beers_tests = service.all_beers
-end
-
-  it 'all key values of name should be of data type string' do
-    p @beers_tests
-    expect(@beers_tests["name"]).to be_a_kind_of(String)
+  before(:all) do
+    service = PunkBeerService.new
+    @beers_tests = service.all_beers_service
   end
-  it 'all key values of id should be of data type Integer' do
-    expect(@beers_tests["id"]).to be_a_kind_of(Integer)
+
+    it 'all key values of name should be of data type string' do
+      @beers_tests.each do |i|
+      expect(i["name"]).to be_a_kind_of(String)
+    end
   end
-  it 'The abv value of all beers should be between 1 and 55' do
-    expect(@beers_tests["abv"]).to be <= 55
+    it 'all key values of id should be of data type Integer' do
+      @beers_tests.each do |i|
+      expect(i["id"]).to be_a_kind_of(Integer)
+    end
+  end
+    it 'The abv value of all beers should be between 1 and 55' do
+      @beers_tests.each do |i|
+      expect(i["abv"]).to be_between(1,55)
+    end
+  end
+    it 'The description should be a string data type' do
+      @beers_tests.each do |i|
+      expect(i["description"]).to be_a_kind_of(String)
+    end
+  end
+    it 'The description value of the boil volume should be an integer' do
+      @beers_tests.each do |i|
+      expect(i["boil_volume"]["value"]).to be_a_kind_of(Integer)
+    end
   end
 end
