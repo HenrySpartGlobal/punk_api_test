586b663b71d468f9fe148d4bd5337802cbdda9dd
HenrySpartaGlobal <henry_koleoso@hotmail.co.uk>
2017-10-24 10:10:49 +0100

    3 working tests

--- punk_beers/spec/single_beer_spec.rb
+++ punk_beers/spec/single_beer_spec.rb
@@ -8,6 +8,13 @@ before(:all) do
 end
 
   it 'all key values of name should be of data type string' do
-    expect(@beers_tests['name']).to be_a_kind_of(String)
+    p @beers_tests
+    expect(@beers_tests["name"]).to be_a_kind_of(String)
+  end
+  it 'all key values of id should be of data type Integer' do
+    expect(@beers_tests["id"]).to be_a_kind_of(Integer)
+  end
+  it 'The abv value of all beers should be between 1 and 55' do
+    expect(@beers_tests["abv"]).to be <= 55
   end
 end
